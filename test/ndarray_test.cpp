0bd8caac23ff75c58df595c879993d21b6ee1e08
fengjiran <fengjiran@foxmail.com>
2025-02-18 21:41:22 +0800

    test api

--- test/ndarray_test.cpp
+++ test/ndarray_test.cpp
@@ -1,13 +1,12 @@
 //
 // Created by richard on 2/9/25.
 //
+#include "runtime/device_api.h"
 #include "runtime/ndarray.h"
 #include "runtime/utils.h"
 
 #include <dmlc/memory_io.h>
 #include <gtest/gtest.h>
-
-#include <runtime/device_api.h>
 
 using namespace litetvm::runtime;
 
@@ -17,13 +16,13 @@ TEST(NDArrayTest, Empty) {
     CHECK(array.IsContiguous());
     std::cout << array.Shape() << std::endl;
 
-    std::string bytes;
-    dmlc::MemoryStringStream stream(&bytes);
-    array.Save(&stream);
+    // std::string bytes;
+    // dmlc::MemoryStringStream stream(&bytes);
+    // array.Save(&stream);
 
     auto* t = array.ToDLPack();
     CHECK_EQ(array.use_count(), 2);
-    std::vector<int64_t> strides {10, 1};
+    std::vector<int64_t> strides{10, 1};
     t->dl_tensor.strides = strides.data();
     CHECK(IsContiguous(t->dl_tensor));
     std::cout << GetDataSize(t->dl_tensor) << std::endl;
@@ -49,4 +48,17 @@ TEST(NDArrayTest, FromExternalDLTensor) {
     const DLTensor* dptr = array.operator->();
     auto t = NDArray::FromExternalDLTensor(*dptr);
     CHECK(array->data == t->data);
+}
+
+TEST(NDArrayTest, AllocArrayAPI) {
+    tvm_index_t shape[] = {2, 5};
+    int ndim = 2;
+    int dtype_code = static_cast<int>(DataType::TypeCode::kFloat);
+    int dtype_bits = 32;
+    int dtype_lanes = 1;
+    int dev_type = static_cast<int>(DLDeviceType::kDLCPU);
+    int dev_id = 0;
+    TVMArrayHandle handle = nullptr;
+    CHECK(TVMArrayAlloc(shape, ndim, dtype_code, dtype_bits, dtype_lanes, dev_type, dev_id, &handle) == 0);
+    CHECK(TVMArrayFree(handle) == 0);
 }
