8ea47f4044eecfe99fbdb6e11bb8c23a3dbc7364
fengjiran <fengjiran@foxmail.com>
2025-02-26 19:35:52 +0800

    add functor

--- include/node/functor.h
+++ include/node/functor.h
@@ -54,6 +54,7 @@ class NodeFunctor<R(const ObjectRef& n, Args...)> {
 public:
     /*! \brief the result type of this functor */
     using result_type = R;
+
     /*!
    * \brief Whether the functor can dispatch the corresponding Node
    * \param n The node to be dispatched
@@ -63,6 +64,7 @@ public:
         uint32_t type_index = n->type_index();
         return type_index < func_.size() && func_[type_index] != nullptr;
     }
+
     /*!
    * \brief invoke the functor, dispatch on type of n
    * \param n The Node argument
@@ -70,10 +72,10 @@ public:
    * \return The result.
    */
     R operator()(const ObjectRef& n, Args... args) const {
-        CHECK(can_dispatch(n)) << "NodeFunctor calls un-registered function on type "
-                               << n->GetTypeKey();
+        CHECK(can_dispatch(n)) << "NodeFunctor calls un-registered function on type " << n->GetTypeKey();
         return (*func_[n->type_index()])(n, std::forward<Args>(args)...);
     }
+
     /*!
    * \brief set the dispatcher for type TNode
    * \param f The function to be set.
@@ -86,10 +88,11 @@ public:
         if (func_.size() <= tindex) {
             func_.resize(tindex + 1, nullptr);
         }
-        ICHECK(func_[tindex] == nullptr) << "Dispatch for " << TNode::_type_key << " is already set";
+        CHECK(func_[tindex] == nullptr) << "Dispatch for " << TNode::_type_key << " is already set";
         func_[tindex] = f;
         return *this;
     }
+
     /*!
    * \brief unset the dispatcher for type TNode
    *
@@ -99,12 +102,57 @@ public:
     template<typename TNode>
     TSelf& clear_dispatch() {// NOLINT(*)
         uint32_t tindex = TNode::RuntimeTypeIndex();
-        ICHECK_LT(tindex, func_.size()) << "clear_dispatch: index out of range";
+        CHECK_LT(tindex, func_.size()) << "clear_dispatch: index out of range";
         func_[tindex] = nullptr;
         return *this;
     }
 };
 
+#define TVM_REG_FUNC_VAR_DEF(ClsName) static TVM_ATTRIBUTE_UNUSED auto& __make_functor##_##ClsName
+
+/*!
+ * \brief Useful macro to set NodeFunctor dispatch in a global static field.
+ *
+ * \code
+ *  // Use NodeFunctor to implement ReprPrinter similar to Visitor Pattern.
+ *  // vtable allows easy patch of new Node types, without changing
+ *  // interface of ReprPrinter.
+ *
+ *  class ReprPrinter {
+ *   public:
+ *    std::ostream& stream;
+ *    // the dispatch function.
+ *    void print(Expr e) {
+ *      const static FType& f = *vtable();
+ *      f(e, this);
+ *    }
+ *
+ *    using FType = NodeFunctor<void (const ObjectRef&, ReprPrinter* )>;
+ *    // function to return global function table
+ *    static FType& vtable();
+ *  };
+ *
+ *  // in cpp/cc file
+ *  ReprPrinter::FType& ReprPrinter::vtable() { // NOLINT(*)
+ *    static FType inst; return inst;
+ *  }
+ *
+ *  TVM_STATIC_IR_FUNCTOR(ReprPrinter, vtable)
+ *  .set_dispatch<Add>([](const ObjectRef& ref, ReprPrinter* p) {
+ *    auto* n = static_cast<const Add*>(ref.get());
+ *    p->print(n->a);
+ *    p->stream << '+'
+ *    p->print(n->b);
+ *  });
+ *
+ *
+ * \endcode
+ *
+ * \param ClsName The name of the class
+ * \param FField The static function that returns a singleton of NodeFunctor.
+ */
+#define TVM_STATIC_IR_FUNCTOR(ClsName, FField) \
+    TVM_STR_CONCAT(TVM_REG_FUNC_VAR_DEF(ClsName), __COUNTER__) = ClsName::FField()
 
 }// namespace litetvm
 
