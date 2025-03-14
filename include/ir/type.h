1bad52175c2ddf81ac737162ac19e681797e731b
fengjiran <fengjiran@foxmail.com>
2025-03-14 15:57:53 +0800

    add type node

--- include/ir/type.h
+++ include/ir/type.h
@@ -252,6 +252,302 @@ public:
     TVM_DEFINE_OBJECT_REF_METHODS(TypeVar, Type, TypeVarNode);
 };
 
+/*!
+ * \brief A global type variable that is used for defining new types or type aliases.
+ * \sa GlobalTypeVar
+ */
+class GlobalTypeVarNode : public TypeNode {
+public:
+    /*!
+     * \brief The name of the variable,
+     *  this only acts as a hint to the user,
+     *  and is not used for equality.
+     */
+    String name_hint;
+    /*! \brief The kind of type parameter */
+    TypeKind kind;
+
+    void VisitAttrs(AttrVisitor* v) {
+        v->Visit("name_hint", &name_hint);
+        v->Visit("kind", &kind);
+    }
+
+    bool SEqualReduce(const GlobalTypeVarNode* other, SEqualReducer equal) const {
+        // name matters for now in global type var.
+        return equal(name_hint, other->name_hint) && equal.FreeVarEqualImpl(this, other);
+    }
+
+    void SHashReduce(SHashReducer hash_reduce) const {
+        hash_reduce(name_hint);
+        hash_reduce.FreeVarHashImpl(this);
+    }
+
+    static constexpr const char* _type_key = "GlobalTypeVar";
+    TVM_DECLARE_FINAL_OBJECT_INFO(GlobalTypeVarNode, TypeNode);
+};
+
+/*!
+ * \brief Managed reference to GlobalTypeVarNode
+ * \sa GlobalTypeVarNode
+ */
+class GlobalTypeVar : public Type {
+public:
+    /*!
+     * \brief Constructor
+     * \param name_hint The name of the type var.
+     * \param kind The kind of the type var.
+     * \param span The span of the type.
+     */
+    // TVM_DLL GlobalTypeVar(String name_hint, TypeKind kind, Span span = Span());
+    LITETVM_API GlobalTypeVar(String name_hint, TypeKind kind);
+
+    TVM_DEFINE_OBJECT_REF_METHODS(GlobalTypeVar, Type, GlobalTypeVarNode);
+};
+
+/*!
+ * \brief The type of tuple values.
+ * \sa TupleType
+ */
+class TupleTypeNode : public TypeNode {
+public:
+    /*! \brief The type of each field in the tuple. */
+    Array<Type> fields;
+
+    TupleTypeNode() {}
+
+    void VisitAttrs(AttrVisitor* v) {
+        v->Visit("fields", &fields);
+        // v->Visit("span", &span);
+    }
+
+    bool SEqualReduce(const TupleTypeNode* other, SEqualReducer equal) const {
+        return equal(fields, other->fields);
+    }
+
+    void SHashReduce(SHashReducer hash_reduce) const { hash_reduce(fields); }
+
+    static constexpr const char* _type_key = "TupleType";
+    TVM_DECLARE_FINAL_OBJECT_INFO(TupleTypeNode, TypeNode);
+};
+
+/*!
+ * \brief Managed reference to TupleTypeNode.
+ * \sa TupleTypeNode.
+ */
+class TupleType : public Type {
+public:
+    /*!
+     * \brief Constructor
+     * \param fields Fields in the tuple.
+     * \param span The span of the type.
+     */
+    LITETVM_API explicit TupleType(Array<Type> fields);
+
+    /*!
+     * \brief Create an empty tuple type that constains nothing.
+     * \return AN empty tuple type.
+     */
+    LITETVM_API TupleType static Empty();
+
+    TVM_DEFINE_OBJECT_REF_METHODS(TupleType, Type, TupleTypeNode);
+};
+
+/*!
+ * \return a type that represents void.
+ */
+inline Type VoidType() {
+    return TupleType::Empty();
+}
+
+/*!
+ * \brief Check whether the type represents void.
+ * \return The check result.
+ */
+inline bool IsVoidType(const Type& type) {
+    auto* n = type.as<TupleTypeNode>();
+    return n && n->fields.size() == 0;
+}
+
+/*!
+ * \brief Potential Constraints in a function.
+ * \sa TypeConstraint
+ */
+class TypeConstraintNode : public TypeNode {
+public:
+    static constexpr const char* _type_key = "TypeConstraint";
+    static constexpr uint32_t _type_child_slots = 1;
+    TVM_DECLARE_BASE_OBJECT_INFO(TypeConstraintNode, TypeNode);
+};
+
+/*!
+ * \brief Managed reference to TypeConstraintNode.
+ * \sa TypeConstraintNode, TypeRelation
+ */
+class TypeConstraint : public Type {
+public:
+    TVM_DEFINE_OBJECT_REF_METHODS(TypeConstraint, Type, TypeConstraintNode);
+};
+
+/*!
+ * \brief Function type.
+ *
+ * We support polymorphic function type.
+ * This can be roughly viewed as template function in C++.
+ *
+ * \sa FuncType, TypeVar, TypeConstraint
+ */
+class FuncTypeNode : public TypeNode {
+public:
+    /*! \brief type type of arguments */
+    Array<Type> arg_types;
+    /*! \brief The type of return value. */
+    Type ret_type;
+    // The following fields are used in polymorphic(template) functions
+    // For normal functions, the following two fields will be empty.
+    /*! \brief The type parameters of the function */
+    Array<TypeVar> type_params;
+    /*!
+     * \brief potential constraint the type need to obey
+     * \note this field is reserved for further purposes.
+     */
+    Array<TypeConstraint> type_constraints;
+
+    void VisitAttrs(AttrVisitor* v) {
+        v->Visit("arg_types", &arg_types);
+        v->Visit("ret_type", &ret_type);
+        v->Visit("type_params", &type_params);
+        v->Visit("type_constraints", &type_constraints);
+        // v->Visit("span", &span);
+    }
+
+    bool SEqualReduce(const FuncTypeNode* other, SEqualReducer equal) const {
+        // type params first as they define type vars.
+        return equal.DefEqual(type_params, other->type_params) &&
+               equal(arg_types, other->arg_types) &&
+               equal(ret_type, other->ret_type) &&
+               equal(type_constraints, other->type_constraints);
+    }
+
+    void SHashReduce(SHashReducer hash_reduce) const {
+        hash_reduce.DefHash(type_params);
+        hash_reduce(arg_types);
+        hash_reduce(ret_type);
+        hash_reduce(type_constraints);
+    }
+
+    static constexpr const char* _type_key = "FuncType";
+    TVM_DECLARE_FINAL_OBJECT_INFO(FuncTypeNode, TypeNode);
+};
+
+/*!
+ * \brief Managed reference to FuncTypeNode.
+ * \sa FuncTypeNode
+ */
+class FuncType : public Type {
+public:
+    /*!
+     * \brief Constructor
+     * \param arg_types The types of the arguments.
+     * \param ret_type The type of the return value.
+     * \param type_params The type parameters.
+     * \param type_constraints The type constraints.
+     * \param span The span information.
+     * \sa FuncTypeNode for more docs about these fields.
+     */
+    LITETVM_API FuncType(Array<Type> arg_types, Type ret_type, Array<TypeVar> type_params,
+                         Array<TypeConstraint> type_constraints);
+
+    TVM_DEFINE_OBJECT_REF_METHODS(FuncType, Type, FuncTypeNode);
+};
+
+
+/*!
+ * \brief Intermediate values that is used to indicate incomplete type
+ *         during type inference.
+ *
+ * If we view the type relations as "computational graph of types",
+ * then IncompleteType represents intermediate values of the graph,
+ * TypeVar represents the input to the graph.
+ *
+ * \sa IncompleteType
+ */
+class IncompleteTypeNode : public TypeNode {
+public:
+    /*! \brief kind of the type. */
+    TypeKind kind;
+
+    void VisitAttrs(AttrVisitor* v) {
+        v->Visit("kind", &kind);
+        // v->Visit("span", &span);
+    }
+
+    bool SEqualReduce(const IncompleteTypeNode* other, SEqualReducer equal) const {
+        return equal(kind, other->kind) && equal.FreeVarEqualImpl(this, other);
+    }
+
+    void SHashReduce(SHashReducer hash_reduce) const {
+        hash_reduce(kind);
+    }
+
+    static constexpr const char* _type_key = "IncompleteType";
+    TVM_DECLARE_FINAL_OBJECT_INFO(IncompleteTypeNode, TypeNode);
+};
+
+/*!
+ * \brief Managed reference to IncompleteTypeNode.
+ * \sa IncompleteTypeNode
+ */
+class IncompleteType : public Type {
+public:
+    /*!
+     * \brief Constructor.
+     * \param kind kind of the type.
+     * \param span The span information.
+     */
+    LITETVM_API explicit IncompleteType(TypeKind kind);
+
+    TVM_DEFINE_OBJECT_REF_METHODS(IncompleteType, Type, IncompleteTypeNode);
+};
+
+/*!
+ * \brief Reference Type High-level Relay IR.
+ *
+ * \sa RelayRefType.
+ */
+class RelayRefTypeNode : public TypeNode {
+public:
+    /*! \brief The type of value in the Reference. */
+    Type value;
+
+    RelayRefTypeNode() = default;
+
+    void VisitAttrs(AttrVisitor* v) {
+        v->Visit("value", &value);
+        // v->Visit("span", &span);
+    }
+
+    bool SEqualReduce(const RelayRefTypeNode* other, SEqualReducer equal) const {
+        return equal(value, other->value);
+    }
+
+    void SHashReduce(SHashReducer hash_reduce) const { hash_reduce(value); }
+
+    // Keep the relay prefix in the type as this type is specific
+    // to the relay itself.
+    static constexpr const char* _type_key = "relay.RefType";
+    TVM_DECLARE_FINAL_OBJECT_INFO(RelayRefTypeNode, TypeNode);
+};
+
+/*!
+ * \brief Managed reference to RelayRefTypeNode.
+ * \sa RelayRefTypeNode.
+ */
+class RelayRefType : public Type {
+public:
+    LITETVM_API explicit RelayRefType(Type value);
+    TVM_DEFINE_OBJECT_REF_METHODS(RelayRefType, Type, RelayRefTypeNode);
+};
+
 
 }// namespace litetvm
 
--- src/ir/type.cpp
+++ src/ir/type.cpp
@@ -46,5 +46,75 @@ TVM_REGISTER_GLOBAL("ir.TypeVar").set_body_typed([](String name, int kind) {
     return TypeVar(name, static_cast<TypeKind>(kind));
 });
 
+GlobalTypeVar::GlobalTypeVar(String name_hint, TypeKind kind) {
+    auto n = runtime::make_object<GlobalTypeVarNode>();
+    n->name_hint = name_hint;
+    n->kind = kind;
+    data_ = std::move(n);
+}
+
+TVM_REGISTER_NODE_TYPE(GlobalTypeVarNode);
+
+TVM_REGISTER_GLOBAL("ir.GlobalTypeVar").set_body_typed([](String name, int kind) {
+    return GlobalTypeVar(name, static_cast<TypeKind>(kind));
+});
+
+TupleType::TupleType(Array<Type> fields) {
+    auto n = runtime::make_object<TupleTypeNode>();
+    n->fields = std::move(fields);
+    data_ = std::move(n);
+}
+
+TupleType TupleType::Empty() {
+    return TupleType(Array<Type>());
+}
+
+TVM_REGISTER_NODE_TYPE(TupleTypeNode);
+
+TVM_REGISTER_GLOBAL("ir.TupleType").set_body_typed([](Array<Type> fields) {
+    return TupleType(fields);
+});
+
+FuncType::FuncType(Array<Type> arg_types, Type ret_type, Array<TypeVar> type_params, Array<TypeConstraint> type_constraints) {
+    auto n = runtime::make_object<FuncTypeNode>();
+    n->arg_types = std::move(arg_types);
+    n->ret_type = std::move(ret_type);
+    n->type_params = std::move(type_params);
+    n->type_constraints = std::move(type_constraints);
+    data_ = std::move(n);
+}
+
+TVM_REGISTER_NODE_TYPE(FuncTypeNode);
+
+TVM_REGISTER_GLOBAL("ir.FuncType")
+        .set_body_typed([](Array<Type> arg_types, Type ret_type, Array<TypeVar> type_params,
+                           Array<TypeConstraint> type_constraints) {
+            return FuncType(arg_types, ret_type, type_params, type_constraints);
+        });
+
+IncompleteType::IncompleteType(TypeKind kind) {
+    auto n = runtime::make_object<IncompleteTypeNode>();
+    n->kind = kind;
+    data_ = std::move(n);
+}
+
+TVM_REGISTER_NODE_TYPE(IncompleteTypeNode);
+
+TVM_REGISTER_GLOBAL("ir.IncompleteType").set_body_typed([](int kind) {
+    return IncompleteType(static_cast<TypeKind>(kind));
+});
+
+RelayRefType::RelayRefType(Type value) {
+    auto n = runtime::make_object<RelayRefTypeNode>();
+    n->value = value;
+    data_ = std::move(n);
+}
+
+TVM_REGISTER_GLOBAL("ir.RelayRefType").set_body_typed([](Type value) {
+    return RelayRefType(value);
+});
+
+TVM_REGISTER_NODE_TYPE(RelayRefTypeNode);
+
 
 }// namespace litetvm
