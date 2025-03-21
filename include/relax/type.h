434080d34ddaa48187aeb2e682d16c0754027146
fengjiran <fengjiran@foxmail.com>
2025-03-21 17:41:52 +0800

    add type

---
+++ include/relax/type.h
@@ -0,0 +1,20 @@
+//
+// Created by 赵丹 on 25-3-21.
+//
+
+#ifndef LITETVM_RELAX_TYPE_H
+#define LITETVM_RELAX_TYPE_H
+
+#include "ir/type.h"
+#include "ir/attrs.h"
+#include "tir/expr.h"
+
+#include <string>
+
+namespace litetvm {
+namespace relax {
+
+}
+}
+
+#endif //LITETVM_RELAX_TYPE_H
---
+++ src/relax/ir/type.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 赵丹 on 25-3-21.
+//
+
+#include "relax/type.h"
+#include "runtime/registry.h"
+
+namespace litetvm {
+namespace relax {
+
+}
+}
+
