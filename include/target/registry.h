c470123613fb34234427bff8328da79cec1fe769
fengjiran <fengjiran@foxmail.com>
2025-04-10 14:49:09 +0800

    add target registry

---
+++ include/target/registry.h
@@ -0,0 +1,8 @@
+//
+// Created by 赵丹 on 25-4-10.
+//
+
+#ifndef REGISTRY_H
+#define REGISTRY_H
+
+#endif //REGISTRY_H
---
+++ src/target/registry.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 赵丹 on 25-4-10.
+//
+
+#include "target/registry.h"
