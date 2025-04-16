e7d7856f3cc09f879856ca25438091b44ae7e28c
fengjiran <fengjiran@foxmail.com>
2025-04-16 14:41:36 +0800

    add transform

---
+++ include/relax/block_builder.h
@@ -0,0 +1,8 @@
+//
+// Created by 赵丹 on 25-4-16.
+//
+
+#ifndef LITETVM_RELAX_BLOCK_BUILDER_H
+#define LITETVM_RELAX_BLOCK_BUILDER_H
+
+#endif //LITETVM_RELAX_BLOCK_BUILDER_H
---
+++ src/relax/ir/block_builder.cpp
@@ -0,0 +1,6 @@
+//
+// Created by 赵丹 on 25-4-16.
+//
+
+#include "relax/block_builder.h"
+#include "relax/transform.h"
